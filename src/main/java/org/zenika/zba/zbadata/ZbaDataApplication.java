58d86b8d93dd410a4fe17d72f2f5e30b3a5706cf
Henri <henribom@hotmail.fr>
2019-03-05 16:32:59 +0100

    Add main file for project

---
+++ src/main/java/org/zenika/zba/zbadata/ZbaDataApplication.java
@@ -0,0 +1,13 @@
+package org.zenika.zba.zbadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZbaDataApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/zenika/zba/zbadata/controller/RecipeController.java
@@ -0,0 +1,77 @@
+package org.zenika.zba.zbadata.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.zenika.zba.zbadata.dao.RecipeDao;
+import org.zenika.zba.zbadata.exception.RecipeNotFindException;
+import org.zenika.zba.zbadata.model.Recipe;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@Api(description = "Receive the CRUD command")
+@RestController
+public class RecipeController {
+
+    @Autowired
+    private RecipeDao recipeDao;
+
+    @GetMapping(value = "/Recipe")
+    public List<Recipe> listRecipe() throws RecipeNotFindException {
+        List<Recipe> recipe = recipeDao.findAll();
+        if(recipe == null) throw new RecipeNotFindException("No recipe find");
+        return recipe;
+    }
+
+    @GetMapping(value = "/Recipe/id{value}")
+    public Recipe findRecipeId(@PathVariable int value) throws RecipeNotFindException {
+        Recipe recipe = recipeDao.findById(value);
+        if(recipe == null) throw new RecipeNotFindException("No recipe find for this id");
+        return recipe;
+    }
+
+    @GetMapping(value = "/Recipe/name{value}")
+    public Recipe findRecipeName(@PathVariable String value) throws RecipeNotFindException {
+        Recipe recipe = recipeDao.findByName(value);
+        if(recipe == null) throw new RecipeNotFindException("No recipe find for this name");
+        return recipe;
+    }
+
+    @GetMapping(value = "/Recipe/ingredientType{value}")
+    public List<Recipe> findRecipeIngredientType(@PathVariable String value) throws RecipeNotFindException {
+        List<Recipe> recipe = recipeDao.findByIngredientType(value);
+        if(recipe == null) throw new RecipeNotFindException("No recipe find");
+        return recipe;
+    }
+
+    @GetMapping(value = "/Recipe/malt{value}")
+    public List<Recipe> findRecipeMalt(@PathVariable String value) throws RecipeNotFindException {
+        List<Recipe> recipe = recipeDao.findByMalt(value);
+        if(recipe == null) throw new RecipeNotFindException("No recipe find");
+        return recipe;
+    }
+
+    @GetMapping(value = "/Recipe/userId{value}")
+    public List<Recipe> findRecipeCreator(@PathVariable String value) throws RecipeNotFindException {
+        List<Recipe> recipe = recipeDao.findByCreator(value);
+        if(recipe == null) throw new RecipeNotFindException("No recipe find");
+        return recipe;
+    }
+
+    @DeleteMapping(value ="/Recipe{value}")
+    public String deleteRecipe(@PathVariable String value) throws RecipeNotFindException {
+        Recipe recipe = recipeDao.findByName(value);
+        if(recipe == null) throw new RecipeNotFindException("No recipe find for this name");
+        System.out.println(value);
+        System.out.println(recipe);
+        recipeDao.delete(recipe);
+        return "Deleted";
+    }
+
+    @PostMapping(value ="/Recipe")
+    public Recipe addRecipe(@RequestBody Recipe recipe) {
+        recipeDao.save(recipe);
+        return recipe;
+    }
+}
